582fadabb0419c2ee0fbe3e5bd5edb4b43717cb7
Oliver Gondža <ogondza@gmail.com>
2021-09-16 10:22:17 +0200

    Improve bat

--- functions/bat.fish
+++ functions/bat.fish
@@ -1,3 +1,3 @@
 function bat --description "Smart cat"
-  command bat --wrap=never --paging=never --style=changes,numbers $argv
+  command bat --wrap=never --paging=never --style=changes,numbers,header $argv
 end
