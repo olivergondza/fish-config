2ff0f67cad129a6f176e7ec6db8f6be2d3874b09
Oliver Gondža <ogondza@gmail.com>
2023-04-18 12:29:49 +0200

    Add oc completion

--- config.fish
+++ config.fish
@@ -3,5 +3,11 @@ complete -c s -w srch
 set -u SHELL fish
 bind \cq history_delete_current_line
 
-set PATH $PATH ~/code/goreutils/bin /home/ogondza/.go/bin
+set PATH $PATH ~/code/goreutils/bin ~/.go/bin
 set -x GOPATH /home/ogondza/.go
+
+# Force newer than whichever is default ATM
+set -x IDEA_JDK /usr/lib/jvm/java-17-openjdk/
+
+# Generate latest origin-client completions
+oc completion fish > ~/.config/fish/completions/oc.fish
