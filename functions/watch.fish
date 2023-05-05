f9e92b3961f9a390bd1b568681bd19b38962187d
Oliver Gondža <ogondza@gmail.com>
2023-05-05 09:37:43 +0200

    Fix multiline watch

--- functions/watch.fish
+++ functions/watch.fish
@@ -2,9 +2,11 @@ function watch --description "Better watch"
   set last ""
   while :;
     #set current "$(eval $argv)" https://github.com/fish-shell/fish-shell/commit/5999d660c00546dea9b00b97722eb11ad9bb4ff7
-    set current (eval $argv)
+    set current (eval $argv | string collect)
     if [ "$current" != "$last" ]
-      date
+      echo -n (set_color red)
+      command date +':: %Y-%m-%d %H:%M:%S ::'
+      echo -n (set_color normal)
       echo -e "$current"
       set last $current
     end
