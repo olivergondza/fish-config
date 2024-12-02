bd0c6a8f0d8b623c1e60b8683e3b99649f9c4828
Oliver Gondža <ogondza@gmail.com>
2024-12-02 09:37:29 +0100

    Use predefined editor for jira

--- functions/jira.fish
+++ functions/jira.fish
@@ -1,3 +1,6 @@
 function jira --description "Run jira with auth on"
-    env JIRA_AUTH_TYPE=bearer JIRA_API_TOKEN=(pass show rh/issues.redhat.com/ogondza@redhat.com/jira-cli) jira $argv
+    env JIRA_AUTH_TYPE=bearer \
+        JIRA_API_TOKEN=(pass show rh/issues.redhat.com/ogondza@redhat.com/jira-cli) \
+        JIRA_EDITOR=vim \
+        jira $argv
 end
