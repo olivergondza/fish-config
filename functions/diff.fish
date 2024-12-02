31bc760cc8815f6c4eecfc013b10dc28c6b9e449
Oliver Gondža <ogondza@gmail.com>
2024-12-02 09:36:54 +0100

    use plan-old git for recursive search

--- functions/diff.fish
+++ functions/diff.fish
@@ -1,3 +1,7 @@
 function diff
-    git diff --no-index $argv
+    if contains -- "-r" $argv; or contains -- "--recursive" $argv
+        command diff --color=auto $argv
+    else
+        git diff --no-index $argv
+    end
 end
