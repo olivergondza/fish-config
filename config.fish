f1e40b9942188815fdd1e6dee7b3954f36db93ca
Oliver Gondža <ogondza@gmail.com>
2025-05-14 13:13:18 +0200

    Move to bundled Idea JDK

--- config.fish
+++ config.fish
@@ -6,9 +6,6 @@ bind \cq history_delete_current_line
 set PATH $PATH ~/code/goreutils/bin ~/.go/bin
 set -x GOPATH /home/ogondza/.go
 
-# Force newer than whichever is default ATM
-set -x IDEA_JDK /usr/lib/jvm/java-17-openjdk/
-
 # Generate dynamic completions
 oc completion fish > ~/.config/fish/completions/oc.fish
 kubectl completion fish > ~/.config/fish/completions/kubectl.fish
