84a6c7650913c6d093843bd30ada74a9b64b6b16
Oliver Gondža <ogondza@gmail.com>
2025-12-10 09:23:56 +0100

    Do not insist on bash for podman_run

--- functions/podman_run.fish
+++ functions/podman_run.fish
@@ -1,4 +1,4 @@
 function podman_run -a image_name -d "Run interactive shell inside a newly started container"
 
-  podman run --interactive --rm --tty $argv /bin/bash
+  podman run --interactive --rm --tty $argv sh
 end
