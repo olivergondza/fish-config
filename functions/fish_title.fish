39f7a6f6222ce0b2bef0ca0ad74051c7707c8b17
Oliver Gondža <ogondza@gmail.com>
2025-12-10 09:24:52 +0100

    Use longer fish title

---
+++ functions/fish_title.fish
@@ -0,0 +1,19 @@
+# Original in /usr/share/fish/functions/fish_title.fish
+function fish_title
+    # If we're connected via ssh, we print the hostname.
+    set -l ssh
+    set -q SSH_TTY
+    and set ssh "["(prompt_hostname | string sub -l 10 | string collect)"]"
+    # An override for the current command is passed as the first parameter.
+    # This is used by `fg` to show the true process name, among others.
+    if set -q argv[1]
+        echo -- $ssh (string sub -l 100 -- $argv[1])
+    else
+        # Don't print "fish" because it's redundant
+        set -l command (status current-command)
+        if test "$command" = fish
+            set command
+        end
+        echo -- $ssh (string sub -l 100 -- $command)
+    end
+end
