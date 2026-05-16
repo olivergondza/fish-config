ede28f2b40c65fba5387b52fcb8a11e74b40bf37
Oliver Gondža <ogondza@gmail.com>
2026-05-16 08:53:36 +0200

    feat: cleanup oc shortcuts
    Signed-off-by: Oliver Gondža <ogondza@gmail.com>

--- functions/oc.fish
+++ functions/oc.fish
@@ -5,35 +5,13 @@ function oc
   end
 
   switch $argv[1]
-    # Switch oc clusters
-    case dno
-      __ocp_sso_token_login https://api.dno.ocp-hub.prod.psi.redhat.com:6443
-
-    case dno-dev
-      __ocp_sso_token_login https://api.dev-dno.ocp-hub.prod.psi.redhat.com:6443
-
-    case bm
-      __ocp_sso_token_login https://api.ocp-c1.prod.psi.redhat.com:6443
-
-    case konflux
-      oc login --server=https://api-toolchain-host-operator.apps.stone-prod-p02.hjvn.p1.openshiftapps.com/workspaces/data-and-operations \
-          --token=(
-              oc oidc-login get-token \
-                  --oidc-issuer-url=https://keycloak-rhtap-auth.apps.stone-prod-p02.hjvn.p1.openshiftapps.com/auth/realms/redhat-external \
-                  --oidc-client-id=cloud-services \
-                  | jq -r '.status.token'
-          )
-    case multilog
-      for pod in (oc get pods | awk '$1 ~ /^'$argv[2]'/ && $3 == "Running" { print $1 }');
-        command oc logs $pod | sed 's/^/'$pod': /'
-      end
     case {y,yaml}
       oc get -o yaml $argv[2..-1] | yq .
     case rsh
       # Set TERM inside the container for `oc rsh`
       env TERM=xterm oc $argv
     case '*'
-      command oc $argv
+      command env KUBECTL_COMMAND=oc kubecolor $argv
   end
 end
 
