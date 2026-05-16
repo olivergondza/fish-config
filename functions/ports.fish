983e455d0a254c9d03c095ba287b5fc03397d56f
Oliver Gondža <ogondza@gmail.com>
2026-05-16 08:52:16 +0200

    feat: print ports in a nicer way
    Signed-off-by: Oliver Gondža <ogondza@gmail.com>

--- functions/ports.fish
+++ functions/ports.fish
@@ -1,4 +1,12 @@
+# Pick PID and port from lsof, use ps to print full process args
 function ports --description "List open ports on localhost"
     # https://unix.stackexchange.com/a/27873/96990
-    sudo lsof -iTCP -sTCP:LISTEN -P -n
+    sudo lsof -iTCP -sTCP:LISTEN -P -n | awk '
+          NR>1 {
+            print $2"\t"$9
+          }
+      ' | xargs -n2 sh -c '
+          printf "%20s  %7s  " "$2" "$1"
+          ps -p "$1" -o args= 2>/dev/null
+        ' sh
 end
