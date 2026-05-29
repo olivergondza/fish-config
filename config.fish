cf96288797c806ea67a05d4338f622439d243723
Oliver Gondža <ogondza@gmail.com>
2026-05-29 10:35:22 +0200

    mise + ssh-agent
    Signed-off-by: Oliver Gondža <ogondza@gmail.com>

--- config.fish
+++ config.fish
@@ -1,5 +1,10 @@
+# Put mise tools on path
+mise activate fish | source
+
 source ~/.srch/completion/srch.fish
 complete -c s -w srch
+
+# ctrl+q: Delete command from fish history
 set -u SHELL fish
 bind \cq history_delete_current_line
 
@@ -10,3 +15,5 @@ set -x GOPATH /home/ogondza/.go
 oc completion fish > ~/.config/fish/completions/oc.fish
 kubectl completion fish > ~/.config/fish/completions/kubectl.fish
 tkn completion fish > ~/.config/fish/completions/tkn.fish
+
+eval "$(ssh-agent -c)"
